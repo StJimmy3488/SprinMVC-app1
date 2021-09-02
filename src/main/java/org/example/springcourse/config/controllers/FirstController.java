b0dd4fc03e77de2fa36008e4880433f8eee18a87
Ilay <ilja.vitohins@gmail.com>
2021-09-02 16:47:23 +0300

    Initial commit

---
+++ src/main/java/org/example/springcourse/config/controllers/FirstController.java
@@ -0,0 +1,28 @@
+package org.example.springcourse.config.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/first")
+public class FirstController {
+
+
+    @GetMapping("/hello")
+    public String helloPage(HttpServletRequest request) {
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+
+        System.out.println("Hi "+name+" "+surname);
+
+        return "first/hello";
+    }
+    @GetMapping("/goodbye")
+    public String goodByePage() {
+        return "first/goodbye";
+    }
+}
